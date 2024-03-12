e45983d532d4c3b8516a68b706ad5b35b4c71de8
Divya Kelaskar <divyakelaskar@gmail.com>
2024-03-12 10:04:11 +0530

    feat: add axios post method to the form ✨

--- Axios Post Demo/src/App.jsx
+++ Axios Post Demo/src/App.jsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [post, setPost] = useState({
+    title: '',
+    body: ''
+  })
 
+  const handleInput = (event) => {
+    setPost({...post, [event.target.name]: event.target.value})
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    axios.post('https://jsonplaceholder.typicode.com/posts', {post})
+    .then(response => console.log(response))
+    .then(err => console.log(err))
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit}>
+        Title : <input type="text" onChange={handleInput} name="title"/>
+        <br/>
+        Post : <input type="text" onChange={handleInput} name="body"/>
+        <br/>
+        <button type='submit'>Submit</button>
+      </form>
     </>
   )
 }
