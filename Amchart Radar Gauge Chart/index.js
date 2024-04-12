8cad304c001a891f00a1acf249035d42fd6073ae
Divya Kelaskar <divyakelaskar@gmail.com>
2024-04-12 11:03:34 +0530

    feat: limit the angles 🚀

--- Amchart Radar Gauge Chart/index.js
+++ Amchart Radar Gauge Chart/index.js
@@ -1,7 +1,10 @@
 var root = am5.Root.new("chartdiv")
 
 var chart = root.container.children.push(
-    am5radar.RadarChart.new(root, {}) 
+    am5radar.RadarChart.new(root, {
+        startAngle: -180,
+        endAngle: 0,
+    }) 
 );
 
 var axis = chart.xAxes.push(
