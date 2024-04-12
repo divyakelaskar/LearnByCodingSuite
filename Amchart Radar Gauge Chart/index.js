46cb333e7408d4e48c8e5a19bd81e6326648e389
Divya Kelaskar <divyakelaskar@gmail.com>
2024-04-12 11:00:49 +0530

    feat: add circular axis 🚀

--- Amchart Radar Gauge Chart/index.js
+++ Amchart Radar Gauge Chart/index.js
@@ -4,4 +4,11 @@ var chart = root.container.children.push(
     am5radar.RadarChart.new(root, {}) 
 );
 
+var axis = chart.xAxes.push(
+    am5xy.ValueAxis.new(root, {
+        min:0,
+        max:100,
+        renderer: am5radar.AxisRendererCircular.new(root, {})  
+    })
+);
 
