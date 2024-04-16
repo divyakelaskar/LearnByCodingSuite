f357c1343d9eecb8664c903e5748533e6c858396
Divya Kelaskar <divyakelaskar@gmail.com>
2024-04-16 12:40:53 +0530

    feat: add chart and cursor ✨

--- Amchart Column Chart/index.js
+++ Amchart Column Chart/index.js
@@ -11,3 +11,19 @@ root.setThemes([
     am5themes_Responsive.new(root)
 ])
 
+var chart = root.container.children.push(am5xy.XYChart.new(root, {
+    panX: false,
+    panY: false,
+    wheelX: "panX",
+    wheelY: "zoomX",
+    paddingLeft: 0
+}));
+
+var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {
+    behavior: "zoomX"
+}));
+
+// dotted line for easy visibility
+cursor.lineY.set("visible", false);
+cursor.lineX.set("visible", false);
+
