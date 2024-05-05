bf968e61e8fbcf7590a96e125357b4f951aaa813
Divya Kelaskar <divyakelaskar@gmail.com>
2024-05-05 21:07:03 +0530

    style: add table borders 💎

--- Realtime Table Update Demo/frontend/src/App.js
+++ Realtime Table Update Demo/frontend/src/App.js
@@ -33,54 +33,54 @@ const App = () => {
         };
     }, []);
 
-  return (
-    <div>
-      <table>
-        <tbody>
-          <tr class='header'>
-            <th>Name</th>
-            <th>Value</th>
-          </tr>
-          <tr>
-            <td>Student 1</td>
-            <td>
-              {students.map((innerdata, index) => (
-                index === 0 && (
-                  <React.Fragment key={innerdata.name} className='TDCSStr'>
-                    <td align='right' class='TDCSS'>{innerdata.marks}</td>
-                  </React.Fragment>
-                )
-              ))}
-            </td>
-          </tr>
-          <tr>
-            <td>Student 2</td>
-            <td>
-              {students.map((innerdata, index) => (
-                index === 1 && (
-                  <React.Fragment key={innerdata.name} className='TDCSStr'>
-                    <td align='right' class='TDCSS'>{innerdata.marks}</td>
-                  </React.Fragment>
-                )
-              ))}
-            </td>
-          </tr>
-          <tr>
-            <td>Student 3</td>
-            <td>
-              {students.map((innerdata, index) => (
-                index === 2 && (
-                  <React.Fragment key={innerdata.name} className='TDCSStr'>
-                    <td align='right' class='TDCSS'>{innerdata.marks}</td>
-                  </React.Fragment>
-                )
-              ))}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+    return (
+      <div>
+        <table>
+          <tbody>
+            <tr className='header'>
+              <th style={{ border: '1px solid black' }}>Name</th>
+              <th style={{ border: '1px solid black' }}>Marks</th>
+            </tr>
+            <tr>
+              <td style={{ border: '1px solid black' }}>Student 1</td>
+              <td style={{ border: '1px solid black' }}>
+                {students.map((innerdata, index) => (
+                  index === 0 && (
+                    <React.Fragment key={innerdata.name} className='TDCSStr'>
+                      <td align='right' className='TDCSS'>{innerdata.marks}</td>
+                    </React.Fragment>
+                  )
+                ))}
+              </td>
+            </tr>
+            <tr>
+              <td style={{ border: '1px solid black' }}>Student 2</td>
+              <td style={{ border: '1px solid black' }}>
+                {students.map((innerdata, index) => (
+                  index === 1 && (
+                    <React.Fragment key={innerdata.name} className='TDCSStr'>
+                      <td align='right' className='TDCSS'>{innerdata.marks}</td>
+                    </React.Fragment>
+                  )
+                ))}
+              </td>
+            </tr>
+            <tr>
+              <td style={{ border: '1px solid black' }}>Student 3</td>
+              <td style={{ border: '1px solid black' }}>
+                {students.map((innerdata, index) => (
+                  index === 2 && (
+                    <React.Fragment key={innerdata.name} className='TDCSStr'>
+                      <td align='right' className='TDCSS'>{innerdata.marks}</td>
+                    </React.Fragment>
+                  )
+                ))}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
 };
 
 export default App;
