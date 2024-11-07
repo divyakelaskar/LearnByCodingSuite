145d6d1928ad55651464d2e34f3763a1e1630d69
Divya Kelaskar <divyakelaskar@gmail.com>
2024-11-07 16:36:08 +0530

    go back quite a fe steps back

--- Amchart Column Chart/index.js
+++ Amchart Column Chart/index.js
@@ -27,23 +27,3 @@ var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {
 cursor.lineY.set("visible", false);
 cursor.lineX.set("visible", false);
 
-var date = new Date();
-date.setHours(0, 0, 0, 0);
-var value = 100;
-
-function generateData() {
-  value = Math.round((Math.random() * 10 - 5) + value);
-  am5.time.add(date, "day", 1);
-  return {
-    date: date.getTime(),
-    value: value
-  };
-}
-
-function generateDatas(count) {
-  var data = [];
-  for (var i = 0; i < count; ++i) {
-    data.push(generateData());
-  }
-  return data;
-}
